76099574b04ce3d037863833a3984d9c66bc702f
Valeria-CPaz <val.paz@gmail.com>
2025-10-14 12:48:23 -0300

    GET Products paged

--- src/main/java/com/example/valeriapaz/ecommerce/services/ProductService.java
+++ src/main/java/com/example/valeriapaz/ecommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.example.valeriapaz.ecommerce.dto.ProductDTO;
 import com.example.valeriapaz.ecommerce.entities.Product;
 import com.example.valeriapaz.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,7 +17,6 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
-
 
     // Escrita didática
     @Transactional(readOnly = true)
@@ -25,13 +27,19 @@ public class ProductService {
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
-
     // Escrita mais curta!!
+//    @Transactional(readOnly = true)
+//    public ProductDTO findById(Long id) {
+//        Product product = repository.findById(id).get();
+//        return new ProductDTO(product);
+//    }
 
-    /*
     @Transactional(readOnly = true)
-    public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).get();
-        return new ProductDTO(product);
-    } */
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
+
+
 }
